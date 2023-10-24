0bcd24e23d6c9e5bb1ee67b52853e611615caaf6
KarimGitHubb <92649894+KarimGitHubb@users.noreply.github.com>
2023-10-24 08:57:35 +0300

    Add files via upload

---
+++ code.py
@@ -0,0 +1,19 @@
+import glob
+from PIL import Image
+
+
+
+# get the path/directory
+src_dir = '/home/karim/images'
+dst_dir = '/home/karim/Newimgs'
+
+
+
+# iterate over files in
+# that directory
+for images in glob.iglob(f'{src_dir}/*'):
+    img = Image.open(images)
+    filename = img.filename[19:-1]
+    img.rotate(90).resize((128,128)).convert('RGB').save('/home/karim/Newimgs/'+str(filename),'JPEG')
+
+
